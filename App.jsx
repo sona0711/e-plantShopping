d0d9bf6833cc3c2783ca1e431fab9a699af14ef6
Sona Kh <sonakh0711@gmail.com>
2026-02-23 23:51:48 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
 src/App.jsx
