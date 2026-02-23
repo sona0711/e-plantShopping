093963afcf0569555050cb6523bf5ba2e6c68d61
Sona Kh <sonakh0711@gmail.com>
2026-02-24 00:03:21 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is a plant shop that offers a variety of indoor and outdoor plants.
+        We aim to bring greenery and freshness to your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
